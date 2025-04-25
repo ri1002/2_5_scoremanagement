e6a0694dc76ea0d9e3235f1ad6e1e11e6cb308c6
amaneko0608 <kmm2459381@stu.o-hara.ac.jp>
2025-04-25 10:22:50 +0900

    クラス表示

--- src/main/TestRegistAction.java
+++ src/main/TestRegistAction.java
@@ -1,25 +1,29 @@
 package main;
 
-import java.io.IOException;
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import bean.School;
+import bean.Student;
 import bean.Teacher;
-import dao.ClassNumDao;;
+import dao.ClassNumDao;
+import dao.StudentDao;
+import tool.Action;;
 
-//クラス
-@WebServlet(urlPatterns={"/main/testregistaction"})
-public class TestRegistAction extends HttpServlet{
+public class TestRegistAction extends Action{
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException{
-
+	@Override
+	public void execute
+		( HttpServletRequest request, HttpServletResponse response
+		) throws Exception{
+		HttpSession session = request.getSession();
+		//Teacher teacher = (Teacher)session.getAttribute("user");
 
 		Teacher teacher = new Teacher();
 		teacher.setId("admin1");
@@ -31,17 +35,54 @@ public class TestRegistAction extends HttpServlet{
 			school.setCd("tes");
 			teacher.setSchool(school);
 
+			// セッションにTeacherオブジェクトを保存
+			session.setAttribute("user", teacher);
 
-		//DBからクラスを全件取得する。
-		ClassNumDao cDao = new ClassNumDao();
-		try{
-			List<String> cList = cDao.filter(teacher.getSchool());
-			request.setAttribute("cList", cList);
-		}catch(Exception e){
-			e.printStackTrace(response.getWriter());
-		}
+			String entYearStr="";//入力された入学年度
+			String classNum=""; //入力されたクラス番号
+			String isAttendStr="";//入力された在学フラグ
+			int entYear = 0;//入学年度
+			boolean isAttend = false;//在学フラグ
+			List<Student> students = null;//学生リスト
+			LocalDate todaysDate = LocalDate.now();//LocalDateインスタンスを取得
+			int year = todaysDate.getYear();//現在の年を取得
+			StudentDao sDao = new StudentDao();//学生Dao
+			ClassNumDao cNumDao = new ClassNumDao();//クラス番号Daoを初期化
+			Map<String, String> errors = new HashMap<>();//エラーメッセージ
 
-		request.getRequestDispatcher("/main/test_regist.jsp").forward(request, response);
+
+			List<String> list = cNumDao.filter(teacher.getSchool());
+
+			if (entYear != 0 && !classNum.equals("0")) {
+				// 入学年度とクラス番号を指定
+				students = sDao.filter(teacher.getSchool(), entYear, classNum, isAttend);
+			} else if (entYear != 0 && classNum.equals("0")) {
+					// 入学年度のみ
+					students = sDao.filter(teacher.getSchool(), entYear, isAttend);
+			} else if (entYear == 0 && (classNum == null ||  classNum.equals("0"))) {
+					//指定なしの場合
+					//全学生情報を取得
+					students = sDao.filter(teacher.getSchool(),isAttend);
+
+			} else {
+				errors.put("errors", "検索条件が正しくありません");
+				//全学生情報を取得
+				students = sDao.filter(teacher.getSchool(), isAttend);
+			}
+
+		//レスポンス値をセット
+		//リクエストに入学年度をセット
+		request.setAttribute("f1", entYear);
+		//リクエストにクラス番号をセット
+		request.setAttribute("f2", classNum);
+		request.setAttribute("f3", isAttendStr);
+
+		//リクエストに学生リストをセット
+		request.setAttribute("students", students);
+		request.setAttribute("class_num_set", list);
+
+		//JSPへフォワード
+				request.getRequestDispatcher("test_regist.jsp").forward(request, response);
 
 	}
 }
