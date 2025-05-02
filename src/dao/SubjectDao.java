964422ccb30264f15f9c4af9fbcceec51dc4a869
djangouser <kmm2459320@stu.o-hara.ac.jp>
2025-05-02 12:30:37 +0900

    微調整

--- src/dao/SubjectDao.java
+++ src/dao/SubjectDao.java
@@ -9,8 +9,88 @@ import java.util.List;
 import bean.School;
 import bean.Subject;
 
-
 public class SubjectDao extends Dao {
+
+    public List<Subject> getAll() throws Exception {
+        List<Subject> subjectList = new ArrayList<>();
+
+        String sql = "SELECT s.cd, s.name, sc.cd AS school_cd, sc.name AS school_name "
+                   + "FROM subject s "
+                   + "JOIN school sc ON s.school_cd = sc.cd";
+
+        try (Connection con = getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                Subject subject = new Subject();
+
+                School school = new School();
+                school.setCd(rs.getString("school_cd"));
+                school.setName(rs.getString("school_name"));
+
+                subject.setCd(rs.getString("cd"));
+                subject.setName(rs.getString("name"));
+                subject.setSchoolCd(rs.getString("school_cd"));
+
+                subjectList.add(subject);
+            }
+        }
+
+        return subjectList;
+    }
+
+    public Subject get(String cd) throws Exception {
+        String sql = "SELECT s.cd, s.name, sc.cd AS school_cd, sc.name AS school_name "
+                   + "FROM subject s "
+                   + "JOIN school sc ON s.school_cd = sc.cd "
+                   + "WHERE s.cd = ?";
+
+        try (Connection con = getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            stmt.setString(1, cd);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    Subject subject = new Subject();
+
+                    School school = new School();
+                    school.setCd(rs.getString("school_cd"));
+                    school.setName(rs.getString("school_name"));
+
+                    subject.setCd(rs.getString("cd"));
+                    subject.setName(rs.getString("name"));
+                    subject.setSchoolCd(rs.getString("school_cd"));
+
+                    return subject;
+                }
+            }
+        }
+
+        return null; // 見つからなかった
+    }
+
+
+    public boolean save(Subject subject) throws Exception {
+        String sql = "INSERT INTO subject(cd, name, school_cd) "
+                   + "VALUES (?, ?, ?) "
+                   + "ON DUPLICATE KEY UPDATE name = ?, school_cd = ?";
+
+        try (Connection con = getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            stmt.setString(1, subject.getCd());
+            stmt.setString(2, subject.getName());
+            stmt.setString(3, subject.getSchoolCd());
+
+            // ON DUPLICATE KEY UPDATE
+            stmt.setString(4, subject.getName());
+            stmt.setString(5, subject.getSchoolCd());
+
+            return stmt.executeUpdate() > 0;
+        }
+    }
+
     public List<Subject> filter(School school) throws Exception {
         List<Subject> list = new ArrayList<>();
 
