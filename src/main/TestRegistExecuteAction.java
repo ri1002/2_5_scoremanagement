89f26382fca38ca8999d0cd5badf06363061ca94
djangouser <kmm2459320@stu.o-hara.ac.jp>
2025-05-09 20:43:03 +0900

    コミット

--- src/main/TestRegistExecuteAction.java
+++ src/main/TestRegistExecuteAction.java
@@ -1,3 +1,4 @@
+
 package main;
 
 import java.util.ArrayList;
@@ -10,8 +11,6 @@ import bean.Student;
 import bean.Subject;
 import bean.Teacher;
 import bean.Test;
-import dao.ClassNumDao;
-import dao.SubjectDao;
 import dao.TestDao;
 import tool.Action;
 
@@ -41,28 +40,12 @@ public class TestRegistExecuteAction extends Action {
             //リスト
             List<Test> list = new ArrayList<>();
 
+
             for (int i = 0; i < regists.length; i++) {
-                int point;
-
-                    point = Integer.parseInt(points[i]);
-
-                if (point < 0 || point > 100) {
-                    request.setAttribute("error", "点数は0～100の範囲で入力してください");
-
-                    SubjectDao subjectDao = new SubjectDao();
-        			List<Subject> subjectList = subjectDao.filter(teacher.getSchool());
-
-        			ClassNumDao cNumDao = new ClassNumDao();//クラス番号Dao
-                    List<String> classNumList = cNumDao.filter(teacher.getSchool());
-
-    	            request.setAttribute("class_num_set", classNumList);
-    	            request.setAttribute("subjects", subjectList);
-                    request.getRequestDispatcher("/main/test_regist.jsp").forward(request, response);
-                    return;
-                }
+                String regist = regists[i];
+                int point = Integer.parseInt(points[i]);
                 int count = Integer.parseInt(counts[i]);
                 String subjectCd = subjects[i];
-                String regist = regists[i];
 
                 Student student = new Student();
                 student.setNo(regist);
@@ -80,10 +63,9 @@ public class TestRegistExecuteAction extends Action {
                 list.add(test);
             }
 
-            // 全員分の点数が有効なら一括登録
             TestDao dao = new TestDao();
             dao.save(list);
 
             request.getRequestDispatcher("/main/test_regist_done.jsp").forward(request, response);
-            }
         }
+    }
