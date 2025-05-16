a54e7004d93df0a1d10204aa3c027c1d0dec7fdc
amaneko0608 <kmm2459381@stu.o-hara.ac.jp>
2025-05-16 12:27:26 +0900

    成績登録

--- src/dao/TestDao.java
+++ src/dao/TestDao.java
@@ -13,41 +13,68 @@ import bean.Test;
 
 public class TestDao extends Dao {
 
-	public Test get(Student student, Subject subject, School school, int no){
-		Test test = new Test();
-		return test;
-
-	}
+    public Test get(Student student, Subject subject, School school, int no) {
+        Test test = new Test();
+        return test;
+    }
 
     public List<Test> filter(int entYear, String classNum, Subject subject, int num, School school) throws Exception {
         List<Test> list = new ArrayList<>();
-        Connection connection = getConnection();
-        PreparedStatement statement = null;
-        ResultSet rSet = null;
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                 "SELECT student.no AS student_no, student.name AS student_name, student.ent_year, student.class_num, " +
+                 "test.no AS test_no, test.point, " +
+                 "subject.cd AS subject_cd, subject.name AS subject_name " +
+                 "FROM student " +
+                 "JOIN test ON student.no = test.student_no " +
+                 "JOIN subject ON subject.cd = test.subject_cd " +
+                 "WHERE student.ent_year = ? " +
+                 "AND student.class_num = ? " +
+                 "AND subject.cd = ? " +
+                 "AND test.no = ? " +
+                 "ORDER BY test.no ASC")) {
 
-        String sql = "SELECT * " +
-                     "FROM student " +
-                     "JOIN test ON student.no = test.student_no " +
-                     "JOIN subject ON subject.cd = test.subject_cd " +
-                     "WHERE student.ent_year = ? " +
-                     "AND student.class_num = ? " +
-                     "AND subject.cd = ? " +
-                     "AND test.no = ? " +
-                     "ORDER BY test.no ASC";
-
-        try {
-            statement = connection.prepareStatement(sql);
             statement.setInt(1, entYear);
             statement.setString(2, classNum);
             statement.setString(3, subject.getCd());
             statement.setInt(4, num);
-            rSet = statement.executeQuery();
-            list = postFilter(rSet, school);
-        } finally {
-            if (statement != null) statement.close();
-            if (rSet != null) rSet.close();
-        }
 
+            try (ResultSet rSet = statement.executeQuery()) {
+                list = postFilter(rSet, school);
+            }
+        }
+        return list;
+    }
+
+    public List<Student> findStudentsWithoutTest(int entYear, String classNum, Subject subject, int num, School school) throws Exception {
+        List<Student> list = new ArrayList<>();
+        String sql = "SELECT * FROM student " +
+                     "WHERE ent_year = ? " +
+                     "AND class_num = ? " +
+                     "AND NOT EXISTS ( " +
+                     "  SELECT * FROM test " +
+                     "  WHERE student.no = test.student_no " +
+                     "  AND subject_cd = ? AND no = ?)";
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, entYear);
+            statement.setString(2, classNum);
+            statement.setString(3, subject.getCd());
+            statement.setInt(4, num);
+
+            try (ResultSet rSet = statement.executeQuery()) {
+                while (rSet.next()) {
+                    Student student = new Student();
+                    student.setNo(rSet.getString("no"));
+                    student.setName(rSet.getString("name"));
+                    student.setEntYear(rSet.getInt("ent_year"));
+                    student.setClassNum(rSet.getString("class_num"));
+                    list.add(student);
+                }
+            }
+        }
         return list;
     }
 
@@ -58,18 +85,18 @@ public class TestDao extends Dao {
 
             Student student = new Student();
             student.setNo(rSet.getString("student_no"));
-            student.setName(rSet.getString("name"));
+            student.setName(rSet.getString("student_name"));
             student.setEntYear(rSet.getInt("ent_year"));
             student.setClassNum(rSet.getString("class_num"));
 
             Subject subject = new Subject();
-            subject.setCd(rSet.getString("cd"));
-            subject.setName(rSet.getString("subject.name"));
+            subject.setCd(rSet.getString("subject_cd"));
+            subject.setName(rSet.getString("subject_name"));
 
             test.setStudent(student);
             test.setSubject(subject);
             test.setClassNum(rSet.getString("class_num"));
-            test.setNo(rSet.getInt("test.no"));
+            test.setNo(rSet.getInt("test_no"));
             test.setPoint(rSet.getInt("point"));
             test.setSchool(school);
 
@@ -80,25 +107,21 @@ public class TestDao extends Dao {
 
     public boolean save(List<Test> list) throws Exception {
         boolean success = true;
-        Connection connection = null;
 
-        try {
-            connection = getConnection();
+        try (Connection connection = getConnection()) {
             connection.setAutoCommit(false);
 
-            for (Test test : list) {
-                save(test, connection);
+            try {
+                for (Test test : list) {
+                    save(test, connection);
+                }
+                connection.commit();
+            } catch (Exception e) {
+                connection.rollback();
+                success = false;
+                throw e;
             }
-
-            connection.commit();
-        } catch (Exception e) {
-            success = false;
-            if (connection != null) connection.rollback();
-            throw e;
-        } finally {
-            if (connection != null) connection.close();
         }
-
         return success;
     }
 
@@ -106,46 +129,49 @@ public class TestDao extends Dao {
         boolean result = false;
 
         String checkSql = "SELECT COUNT(*) FROM test WHERE student_no = ? AND subject_cd = ? AND no = ?";
-        String insertSql = "INSERT INTO test (student_no, subject_cd, no, point) VALUES (?, ?, ?, ?)";
-        String updateSql = "UPDATE test SET point = ? WHERE student_no = ? AND subject_cd = ? AND no = ?";
+        String insertSql = "INSERT INTO test (student_no, subject_cd, no, point, class_num, school_cd) VALUES (?, ?, ?, ?, ?, ?)";
+        String updateSql = "UPDATE test SET point = ?, class_num = ? WHERE student_no = ? AND subject_cd = ? AND no = ?";
         String deleteSql = "DELETE FROM test WHERE student_no = ? AND subject_cd = ? AND no = ?";
 
-        try (
-            PreparedStatement checkStmt = connection.prepareStatement(checkSql);
-            PreparedStatement insertStmt = connection.prepareStatement(insertSql);
-            PreparedStatement updateStmt = connection.prepareStatement(updateSql);
-            PreparedStatement deleteStmt = connection.prepareStatement(deleteSql)
-        ) {
+        try (PreparedStatement checkStmt = connection.prepareStatement(checkSql);
+             PreparedStatement insertStmt = connection.prepareStatement(insertSql);
+             PreparedStatement updateStmt = connection.prepareStatement(updateSql);
+             PreparedStatement deleteStmt = connection.prepareStatement(deleteSql)) {
+
             checkStmt.setString(1, test.getStudent().getNo());
             checkStmt.setString(2, test.getSubject().getCd());
             checkStmt.setInt(3, test.getNo());
 
-            ResultSet rs = checkStmt.executeQuery();
-            rs.next();
-            int count = rs.getInt(1);
+            try (ResultSet rs = checkStmt.executeQuery()) {
+                rs.next();
+                int count = rs.getInt(1);
 
-            if (test.getPoint() == null) {
-                if (count > 0) {
-                    deleteStmt.setString(1, test.getStudent().getNo());
-                    deleteStmt.setString(2, test.getSubject().getCd());
-                    deleteStmt.setInt(3, test.getNo());
-                    result = deleteStmt.executeUpdate() == 1;
+                if (test.getPoint() == null) {
+                    if (count > 0) {
+                        deleteStmt.setString(1, test.getStudent().getNo());
+                        deleteStmt.setString(2, test.getSubject().getCd());
+                        deleteStmt.setInt(3, test.getNo());
+                        result = deleteStmt.executeUpdate() == 1;
+                    } else {
+                        result = true;
+                    }
                 } else {
-                    result = true;
-                }
-            } else {
-                if (count > 0) {
-                    updateStmt.setInt(1, test.getPoint());
-                    updateStmt.setString(2, test.getStudent().getNo());
-                    updateStmt.setString(3, test.getSubject().getCd());
-                    updateStmt.setInt(4, test.getNo());
-                    result = updateStmt.executeUpdate() == 1;
-                } else {
-                    insertStmt.setString(1, test.getStudent().getNo());
-                    insertStmt.setString(2, test.getSubject().getCd());
-                    insertStmt.setInt(3, test.getNo());
-                    insertStmt.setInt(4, test.getPoint());
-                    result = insertStmt.executeUpdate() == 1;
+                    if (count > 0) {
+                        updateStmt.setInt(1, test.getPoint());
+                        updateStmt.setString(2, test.getClassNum());
+                        updateStmt.setString(3, test.getStudent().getNo());
+                        updateStmt.setString(4, test.getSubject().getCd());
+                        updateStmt.setInt(5, test.getNo());
+                        result = updateStmt.executeUpdate() == 1;
+                    } else {
+                        insertStmt.setString(1, test.getStudent().getNo());
+                        insertStmt.setString(2, test.getSubject().getCd());
+                        insertStmt.setInt(3, test.getNo());
+                        insertStmt.setInt(4, test.getPoint());
+                        insertStmt.setString(5, test.getClassNum());
+                        insertStmt.setString(6, test.getSchool().getCd());
+                        result = insertStmt.executeUpdate() == 1;
+                    }
                 }
             }
         }
